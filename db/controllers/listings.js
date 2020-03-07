6b63cfc283acdd065a06242442ce1102888d182b
Sage-Kearney <56275819+Sage-Kearney@users.noreply.github.com>
2020-03-06 22:31:38 -0500

    data is seeded, data is rendering in browser,  vendorID is null

--- db/controllers/listings.js
+++ db/controllers/listings.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const sequelize = require('sequelize');
-const Vendor = require('../models/Vendor');
+const Listing = require('../models/Listing');
 
-// router
-//   .get('/', (req, res) => {
-//     Vendor.findAll().then(vendors => res.json(vendors));
-//   })
-//   .catch(err => console.error(err));
+router.get('/', async (req, res) => {
+  const listings = await Listing.findAll();
+  return res.json(listings);
+});
 
 module.exports = router;
