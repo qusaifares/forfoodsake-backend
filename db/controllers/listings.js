0159e848b13e38ca26de312f64f5650d059c4d5c
Qusai Fares <qusaifares@gmail.com>
2020-09-20 17:33:01 -0400

    refactor: change req body in server requests
    changed vendor edit and listing edit routes
    req body is now object with fieldName: value format

--- db/controllers/listings.js
+++ db/controllers/listings.js
@@ -1,44 +1,67 @@
 const express = require('express');
 const router = express.Router();
-const sequelize = require('sequelize');
 const Listing = require('../../models').Listing;
 const Vendor = require('../../models').Vendor;
 // const upload = require('./aws');
 
 router.get('/', async (req, res) => {
+  try {
+    console.log('hello');
     const listings = await Listing.findAll();
+    console.log(listings);
+    if (!listings) throw new Error('No listings found');
     res.json(listings);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 router.get('/:id', async (req, res) => {
+  try {
     const listing = await Listing.findByPk(req.params.id);
+    if (!listing) throw new Error('Listing not found');
     res.json(listing);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 router.post('/:vendorId/new/', async (req, res) => {
+  try {
     // req.body should be listing minus VendorId/timestamps
     const timestamp = Date.now();
     const vendor = await Vendor.findByPk(req.params.vendorId);
     const listing = await vendor.createListing({
-        ...req.body,
-        createdAt: timestamp,
-        updatedAt: timestamp
+      ...req.body,
+      createdAt: timestamp,
+      updatedAt: timestamp,
     });
     res.json(listing);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 router.put('/:id/edit', async (req, res) => {
+  try {
     // req.body should look like this: { field: field being edited e.g.'price', value: 5.99 }
-    const listingToUpdate = await Listing.findByPk(req.params.id);
-    listingToUpdate[req.body.field] = req.body.value;
-    listingToUpdate.save();
-    return res.json(listingToUpdate);
+    const listingToUpdate = await Listing.update(req.body, {
+      where: { id: req.params.id },
+    });
+    res.json(listingToUpdate);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 router.delete('/:id/delete', async (req, res) => {
+  try {
     const listingToDelete = await Listing.findByPk(req.params.id);
     await listingToDelete.destroy();
     res.send('Deleted listing with id ' + req.params.id);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 module.exports = router;
--- db/controllers/vendors.js
+++ db/controllers/vendors.js
@@ -5,83 +5,82 @@ const Listing = require('../../models').Listing;
 const bcrypt = require('bcrypt');
 
 router.get('/', async (req, res) => {
-    try {
-        const vendors = await Vendor.findAll({
-            include: [Listing]
-        });
-        res.json(vendors);
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    const vendors = await Vendor.findAll({
+      include: [Listing],
+    });
+    res.json(vendors);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 router.get('/:id', async (req, res) => {
-    const vendor = await Vendor.findByPk(req.params.id, {
-        include: [Listing]
-    });
-    return res.json(vendor);
+  const vendor = await Vendor.findByPk(req.params.id, {
+    include: [Listing],
+  });
+  return res.json(vendor);
 });
 
 router.post('/new', async (req, res) => {
-    // new vendor info
-    let vendorToCreate = req.body;
-    try {
-        // hashing password
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        // replacing password with hashed password
-        vendorToCreate.password = hashedPassword;
+  // new vendor info
+  let vendorToCreate = req.body;
+  try {
+    // hashing password
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    // replacing password with hashed password
+    vendorToCreate.password = hashedPassword;
 
-        const newVendor = await Vendor.create(vendorToCreate);
-        res.json(newVendor);
-    } catch {
-        res.status(500).send();
-    }
+    const newVendor = await Vendor.create(vendorToCreate);
+    res.json(newVendor);
+  } catch {
+    res.status(500).send();
+  }
 
-    // req.body should be the vendor object
+  // req.body should be the vendor object
 });
 
 router.post('/login', async (req, res) => {
-    // req.body should be {email, password}
-    let vendorList = await Vendor.findAll({
-        include: [Listing],
-        where: {
-            email: req.body.email
-        }
-    });
-    if (vendorList.length === 0) {
-        // if query finds nothing
-        res.statusMessage = 'User not found';
-        res.status(400).end();
-    } else {
-        try {
-            vendor = vendorList[0];
-            if (await bcrypt.compare(req.body.password, vendor.password)) {
-                res.json(vendor);
-            } else {
-                // if password doesn't match
-                console.log('bad password');
-                res.statusMessage = 'Current password does not match';
-                res.status(403).end();
-            }
-        } catch {
-            res.status(500).send('Forbidden');
-        }
+  // req.body should be {email, password}
+  let vendorList = await Vendor.findAll({
+    include: [Listing],
+    where: {
+      email: req.body.email,
+    },
+  });
+  if (vendorList.length === 0) {
+    // if query finds nothing
+    res.statusMessage = 'User not found';
+    res.status(400).end();
+  } else {
+    try {
+      vendor = vendorList[0];
+      if (await bcrypt.compare(req.body.password, vendor.password)) {
+        res.json(vendor);
+      } else {
+        // if password doesn't match
+        console.log('bad password');
+        res.statusMessage = 'Current password does not match';
+        res.status(403).end();
+      }
+    } catch {
+      res.status(500).send('Forbidden');
     }
+  }
 });
 
 router.put('/:id/edit', async (req, res) => {
-    // req.body should look like this: { field: field being edited e.g.'email', value: 'new_email@gmail.com' }
-    const vendorToUpdate = await Vendor.findByPk(req.params.id);
-    vendorToUpdate[req.body.field] = req.body.value;
-    vendorToUpdate.save();
-    return res.json(vendorToUpdate);
+  const vendorToUpdate = await Vendor.update(req.body, {
+    where: { id: req.params.id },
+  });
+  return res.json(vendorToUpdate);
 });
 
 router.delete('/:id/delete', async (req, res) => {
-    const vendorToDelete = await Vendor.findByPk(req.params.id);
-    await vendorToDelete.destroy();
-    res.statusMessage = 'Deleted vendor with id ' + req.params.id;
-    res.status(200).end();
+  const vendorToDelete = await Vendor.findByPk(req.params.id);
+  await vendorToDelete.destroy();
+  res.statusMessage = 'Deleted vendor with id ' + req.params.id;
+  res.status(200).end();
 });
 
 module.exports = router;
